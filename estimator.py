56ef4d5b1c4365ef72703ea29c7d07e2c5a8134e
ehernandezvilla <ehernandezvilla@gmail.com>
2018-06-27 00:23:12 -0400

    Create estimator.py

---
+++ estimator.py
@@ -0,0 +1,41 @@
+# https://developer.uber.com/docs/riders/ride-requests/tutorials/api/python #
+
+#OPEN & IMPORT
+
+import numpy as np
+import pandas as pd
+from uber_rides session import Session
+from uber_rides.client import UberRidesClient
+
+session = Session(server_token='TOKEN')
+client = UberRidesClient(session)
+
+#GET A LIST OF AVAILABLE PRODUCTS
+
+response = client.get_products(37.77, -122.41)
+products = response.json.get('products')
+
+#GET PRICE AND TIMES ESTIMATES
+
+response = client.get_products(37.77, -122.41)
+product = response.json.get('products')
+
+response = client.get_price_estimates(
+  start_latitude=37.770,
+  start_longitude=-122.411,
+  end_laltitude=37.791,
+  end_longitude=-122.405,
+  seat_count=2
+)
+
+estimate = response.json.get('prices')
+
+#BUILD DATAFRAME
+
+df = pd.DataFrame(estimate)
+
+#EXPORT TO CSV
+
+df.to_csv('out.csv')
+
+
