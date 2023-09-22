e5258d8f2daa17097c1ddd2dbe2b8943a5018067
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2023-09-22 16:14:07 -0300

    Update estimator.py
    code: refactoring

--- estimator.py
+++ estimator.py
@@ -1,92 +1,67 @@
-
-# References 
-
-#https://www.coordenadas-gps.com/
-#https://developer.uber.com/docs/riders/ride-requests/tutorials/api/python
-
 import mysql.connector
 import numpy as np
 import pandas as pd
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
-from statistics import mean
-import csv
-import json
-import pprint
-from pandas.io.json import json_normalize
 from time import sleep
-import os
 import datetime
 import time
 
-# Addresses
+def get_uber_estimate(sla, slo, ela, elo):
+    session = Session(server_token='TOKEN')
+    client = UberRidesClient(session)
+    response = client.get_price_estimates(
+        start_latitude=sla,
+        start_longitude=slo,
+        end_latitude=ela,
+        end_longitude=elo,
+        seat_count=2
+    )
+    return response.json.get('prices')
 
-# Parque Antonio Rabat 6443, Vitacura 
-sla = -33.371552
-slo = -70.57803209999997
+def convert_to_dataframe(estimate):
+    return pd.DataFrame.from_dict(pd.json_normalize(estimate), orient='columns')
 
-# Joaquin Montero 3000
-ela = -33.4046849
-elo = -70.59926669999999
+def calculate_metrics(df):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    name = df.loc[0][1]
+    distance = df.loc[0][2]
+    duration = df.loc[0][3] / 60
+    low = np.float64(df.loc[0][7]).item()
+    high = np.float64(df.loc[0][5]).item()
+    avg = np.mean([low, high])
+    surge = np.float64(df.loc[0][10]).item()
+    return (name, distance, duration, low, high, avg, surge, st)
 
-for i in range(3000):
-  session = Session(server_token='TOKEN')
-  client = UberRidesClient(session)
-  #Establishing the connection to a Mysql DB
-  cnx = mysql.connector.connect(user='your_user',database='your_db',password='your_pass',host='localhost')
-  cursor = cnx.cursor()
-  response = client.get_price_estimates(
-  start_latitude= sla,
-  start_longitude= slo,
-  end_latitude= ela,
-  end_longitude= elo,
-  seat_count=2
-  )
-  estimate = response.json.get('prices')
-  # Converting the Json call to a numpy dataframe
-  df = pd.DataFrame.from_dict(json_normalize(estimate), orient='columns')
-  ts = time.time() #Timestamp definition
-  st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') #Timestamp definition
-  # Name of the category
-  name = df.loc[0][1] 
-  # Distance of the route in KM
-  distance = df.loc[0][2]
-  # Duration in seg 
-  duration = df.loc[0][3]
-  dur = (duration/60)
-  # Low estimate with float conversion throught a.item() is possible to use np.asscalar(a) as well
-  low = np.float64(df.loc[0][7]).item() 
-  # Hight estimate with float conversion throught a.item() is possible to use np.asscalar(a) as well
-  hight = np.float64(df.loc[0][5]).item() 
-  # Range estimate
-  range = df.loc[0][4] 
-  # (%) Variation between the lowest and the highest value
-  porcen = ((hight-low)/low)
-  # Mean between the lowest and the highest value
-  a = np.array(low)
-  b = np.array(hight)
-  avg = np.mean([a,b])
-  # Surge price
-  surge = np.float64(df.loc[0][10]).item()
-  # Printing the values selected in the dataframe
-  pp = print(name,"|","Dist.=",distance,"|","Durat.=",dur,"|","Low=",low,"|","Hight=",hight,"|",
-  "Range=",range,"|","%=",porcen,"|","AVG=",avg,"|","Surge=",surge,"|",st, end='\n')
-  # List storage for the values
-  list = [name, low, hight, range, porcen,avg,st]
-  # Define the estructure of the table/data
-  add_values = ("INSERT INTO uber_request "
-  "(display_name, duration, distance, estimate, low_estimate, hight_estimate, average, surge) "
-  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-  data_values = (name, dur, distance, range, low, hight, avg, surge)
-  #writer.writerow(list) -> close the loop with the data for a csv file
-  # Insert new value
-  cursor.execute(add_values, data_values)
-  # Making data committed to the database & close the connection after the loop
-  cnx.commit()
-  cursor.close()
-  cnx.close()
-  # Delay definition
-  sleep(0.5)   
+def insert_into_db(cursor, data):
+    add_values = ("INSERT INTO uber_request "
+                  "(display_name, duration, distance, estimate, low_estimate, hight_estimate, average, surge) "
+                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+    cursor.execute(add_values, data)
 
-print("Loop end .....")
-# os.system('cls') -> Clear with system tools
+if __name__ == "__main__":
+    sla = -33.371552
+    slo = -70.57803209999997
+    ela = -33.4046849
+    elo = -70.59926669999999
+
+    for i in range(3000):
+        cnx = mysql.connector.connect(user='your_user', database='your_db', password='your_pass', host='localhost')
+        cursor = cnx.cursor()
+        
+        estimate = get_uber_estimate(sla, slo, ela, elo)
+        df = convert_to_dataframe(estimate)
+        data = calculate_metrics(df)
+        
+        print(f"{data[0]} | Dist.={data[1]} | Durat.={data[2]} | Low={data[3]} | High={data[4]} | AVG={data[5]} | Surge={data[6]} | {data[7]}")
+        
+        insert_into_db(cursor, data)
+        
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        
+        sleep(0.5)
+
+    print("Loop end .....")
