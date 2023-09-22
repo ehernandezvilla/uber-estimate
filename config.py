6c37d60a76faf44c7576608556855e54c6caab56
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2023-09-22 16:38:56 -0300

    Create config.py
    code: adding config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# MySQL Database Configuration
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'your_user'
+MYSQL_PASSWORD = 'your_pass'
+MYSQL_DATABASE = 'your_db'
+
+# Uber API Configuration
+UBER_API_TOKEN = 'YOUR_UBER_API_TOKEN'
+
+# Other Configuration
+# Add any other configuration variables that may be needed
